8996084c91519d7a1fe2d1bd7d682ee5733d752c
Kyle Carter <kcarter134@yahoo.com>
2022-12-12 02:16:18 -0500

    functioning clear button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,19 @@
 const btn_click = document.querySelector('#click-btn');
-const clearBtn = document.querySelector('#clear');
 const picDrop = document.getElementsByClassName('click-cont')[0]; // grabs original containers class and sends picture to an array
+const picture = document.createElement('span'); // creates image container
 
 btn_click.addEventListener('click', () => {
-    const picture = document.createElement('span'); // creates image container
     const clickImg = document.createElement('img'); // creates image tag upon click
     clickImg.src = "./assets/images/city.jpg"; // pulls in picture
     picture.className = 'click-pic'; // sets image containers class
     clickImg.className = 'image-1'; // sets image class
     picture.appendChild(clickImg) //appends image inside of image container
     picDrop.appendChild(picture); // appends image container inside of designated container
-
-    btn_click.disabled = true;
+    
 });
 
+const clearBtn = document.querySelector('#clear');
+
 clearBtn.addEventListener('click', () => {
-    picDrop.length = "";
-    clearBtn.disabled = true;
+    picture.remove();
 })
